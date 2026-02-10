601347a8b734e9ec3410703aea40e1fed2d57dae
olibasquette <olibasquette@gmail.com>
2026-02-10 21:57:49 +0000

    modified gitignore

--- symbolic_regression.py
+++ symbolic_regression.py
@@ -4,24 +4,20 @@ import scipy.io as sio
 from pysr import PySRRegressor
 
 read_data = sio.loadmat('halo_tau_grid.mat') # dimensions (n_freq, n_xHI, n_halo_masses, n_redshifts)
-tau_grid = read_data['tau_grid'] # dimensions (n_freq, n_xHI, n_halo_masses, n_redshifts)
+tau_grid = read_data['tau_arr'] # dimensions (n_freq, n_xHI, n_halo_masses, n_redshifts)
 coarse_freqs = read_data['freq_bins'].flatten() # dimensions (n_freq)
 xHI_bins = read_data['xHI_bins'].flatten() # dimensions (n_xHI)
 halo_masses_Msun = read_data['halo_masses_Msun'].flatten() # dimensions (n_halo_masses)
 redshift_bins = read_data['redshift_bins'].flatten() # dimensions (n_redshifts)
 
 model = PySRRegressor(
-    niterations=1000,
+    niterations=50,
     binary_operators=["+", "-", "*", "/"],
     unary_operators=["exp", "log", "sqrt"],
     extra_sympy_mappings={"exp": "exp", "log": "log", "sqrt": "sqrt"},
     model_selection="best",
     loss="loss(x, y) = (x - y)^2",
     maxsize=20,
-    maxdepth=5,
-    populations=20,
-    ncyclesperiteration=100,
-    random_state=0,
 )
 
 # Reshape the data for PySR
